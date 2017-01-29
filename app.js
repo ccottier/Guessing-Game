9d6dfa12caec99ee433c331b0443c5046e838302
ccottier <chrystiecottier@gmail.com>
2017-01-28 16:30:26 -0800

    updated code, is working now

--- app.js
+++ app.js
@@ -1,77 +1,70 @@
-function myFunction() {
-  var userName = prompt('What\'s Your Name?');
-  alert('Hello '+ userName +'\! Thanks for coming to my first website. Let\'s play a game so that you can get to know me a better.');
-}
-myFunction()
+
+var userName = prompt('What\'s Your Name?');
+alert('Hello '+ userName +'\! Thanks for coming to my first website. Let\'s play a game so that you can get to know me a better.');
+
+// myFunction()
+
+var myAnswerOne = 'Pennsylvania';
 
 
 var answerOne = prompt('In which state was I born?');
-var myAnswerOne = 'Pennsylvania';
 
 if (answerOne === myAnswerOne) {
   alert('You\'re correct! I was born in the oldest hospital in the United States which was founded by Ben Franklin.');
 } else {
   alert('Sorry, that was incorrect.  I was born in Philadelphia, PA.');
 }
-myAnswerOne()
 
 
-function myPet() {
-  var answerTwo = prompt('Do I prefer cats to dogs?');
-  var myAnswerTwo = 'No';
-}
 
-if (answerTwo === myAnswerTwo) {
-  alert('Correct. I love cats.');
+var pet = prompt('Do I prefer dogs to cats?');
+var myPet = 'NO';
+
+if (pet.toUpperCase() === myPet) {
+  alert('Correct! I love cats.');
 } else {
-  alert('Nope. I much prefer cats over dogs.');
+  alert('Nope! I much prefer cats over dogs.');
 }
-myPet()
 
 
 var answerThree = prompt('Am I a morning person?');
-var myAnswerThree = 'No';
+var myAnswerThree = 'NO';
 
-if (answerThree === myAnswerThree) {
+if (answerThree.toUpperCase === myAnswerThree) {
   alert('Correct! I\'m more of a night owl.');
 } else {
-  alert('Incorrect. I\'m not an early bird.');
-}
-myAnswerThree()
-
-
-function myColor() {
-  var answerFour = prompt('Is red my favorite color?');
-  myAnswerFour = 'No';
+  alert('Incorrect! I\'m not an early bird.');
 }
 
-if (answerFour === myAnswerFour) {
+
+var answerFour = prompt('Is purple my favorite color?');
+var myAnswerFour = 'NO';
+
+if (answerFour.toUpperCase === myAnswerFour) {
   alert('Correct! I love green.');
 } else {
   alert('Nice try, but green is actually my favorite color.');
 }
-myAnswerFour()
+// myAnswerFour()
 
 
-function myHoliday () {
-  var answerFive = prompt('Is Halloween my favorite holiday?');
-}
-myAnswerFive = ['Yes', || 'yes', || 'YES', || 'y', or 'Y'];
-if(answerFive ==== myAnswerFive) {
-  console.log('Correct! Halloween is my favorite holiday.  I love costume parties!');
-} else {
-  console.log('Actually, my favorite holiday is Halloween!');
-}
-myHoliday ()
+// var answerFive = prompt('Is Halloween my favorite holiday?');
+// myAnswerFive = ['Yes', || 'yes', || 'YES', || 'y', or 'Y'];
+//
+// if(answerFive ==== myAnswerFive) {
+//   console.log('Correct! Halloween is my favorite holiday.  I love costume parties!');
+// } else {
+//   console.log('Actually, my favorite holiday is Halloween!');
+// }
+// myHoliday ()
 
 
-function myBeverages () {
-  var answerSix = prompt('What is one of my favorite beverages?');
-}
-myAnswerSix = ['kombucha', 'green smoothies', 'coconut water', 'herbal tea'];
+var answerSix = prompt('What is one of my favorite beverages?');
+var myAnswerSix = ['kombucha', 'green smoothies', 'coconut water', 'herbal tea'];
+
 if(answerSix === myAnswerSix) {
   console.log('You guessed correct! My favorite beverages are kombucha, green smoothies, coconut water, and herbal tea.');
 } else {
   console.log('That is incorrect.  We must have different taste in drinks.');
 }
-myBeverages ()
+// myBeverages ()
