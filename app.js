25a8a501508637c5e3eb3462d8512b930adf7d93
ccottier <chrystiecottier@gmail.com>
2017-02-04 15:52:10 -0800

    Fised vegan function to run, finally!

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 var count = 0;
-// var correctAnswers = [];
-
-// function user() {
-//Try to use an iffe to make last alert in code work//
+var totalQuestions = 9;
 
 var userName = prompt('What\'s Your Name?');
 alert('Hello '+ userName +'\! Thanks for coming to my first website. Let\'s play a game so that you can get to know me better.');
@@ -68,6 +65,7 @@ function halloween(){
 
 halloween();
 
+
 function green(){
 
   var color = prompt('Is purple my favorite color?');
@@ -109,103 +107,67 @@ function kombucha(){
     console.log('You\'re correct! My favorite beverages are kombucha, green smoothies, coconut water, and herbal tea.');
     count++;
   } else {
+    alert('That is incorrect.  We must have different taste in drinks.');
     console.log('That is incorrect.  We must have different taste in drinks.');
   }
 }
 
 kombucha();
 
-//
-// function checkGuess() {
-//   var yearsVegan = 18;
-//   for(var v = 0; v < 4; v++) {
-//   // var counter = 0;
-//   var guess = prompt('Can you guess how many years I have been vegan?');
-//   while ((v < 4) && (guess !== yearsVegan)) {
-//     if(guess < yearsVegan) {
-//       alert('You\'re guess is too low.');
-//     }
-//     if(guess > yearsVegan) {
-//       alert('You\'re guess is too high');
-//     }
-//     if(guess === yearsVegan){
-//       alert('You\'re correct! Great guess.');
-//       // count++;
-//     } else {
-//       alert('Sorry, you ran out of guesses. I have been a vegan for 18 years.');
-//     }
-//   }
-// }
-// checkGuess();
-
-// function checkGuess() {
-//   var yearsVegan = 18;
-//   var counter = 0;
-//   var guess = prompt('Can you guess how many years I have been vegan? You have 4 guesses');
-//   while ((counter < 4) && (guess !== yearsVegan)) {
-//     counter++;
-//     if(guess < yearsVegan) {
-//       alert('You\'re guess is too low.');
-//       counter++;
-//       } else if (guess > yearsVegan) {
-//         alert('You\'re guess is too high');
-//         // counter++;
-//       } else if (guess === yearsVegan) {
-//         alert('You\'re correct! Great guess.');
-//       // count++;
-//       } else {
-//         alert('Sorry, you ran out of guesses. I have been a vegan for 18 years.');
-//       }
-//   }
-// }
-// checkGuess();
 
 function vegan() {
   var yearsVegan = 18;
-  var numGuesses = 0; //Keeps track of # of user Guesses.
+  var numGuesses = 0;
 
-  while(numGuesses < 4) {
-    var userGuess = parseInt(prompt('Can you guess how long I have been a vegan?\n(You have ' + (4 - numGuesses) + ' guesses left.)'));
-    if(userGuess === yearsVegan) {
-      alert('You guessed correctly! I have been a vegan for 18 years.');
-      numGuesses = 4;
-      count++;
-    } else if(userGuess < yearsVegan) {
-      alert('Too low! Guess again.');
-    } else if(userGuess > yearsVegan) {
-      alert('Too high! Guess again.');
-    } else {
-      alert('You ran out of guesses. I have been a vegan for 18 years.');
+  for(var v = 0; v < 4; v++) {
+    var userGuess = parseInt(prompt('Can you guess how long I have been a vegan?\n(You have ' + (4 - v) + ' guesses left.)'));
+
+    if(userGuess < yearsVegan) {
+      alert('You\'re guess is too low.');
+      console.log(userGuess);
+      console.log(userGuess < yearsVegan);
     }
-    numGuesses++;
+    else if(userGuess > yearsVegan) {
+      alert('You\'re guess is too high');
+      console.log(userGuess);
+      console.log(userGuess > yearsVegan);
+    }
+    else if(userGuess === 18) {
+      alert('You\'re correct! Great guess.');
+      count++;
+      v = 5;
+      // Added something to make the for loop end
+    }
+  }
+  if((userGuess !== 18) && (numGuesses > 4)) {
+    alert('Sorry, you ran out of guesses. I have been a vegan for 18 years.');
   }
 }
-// (userGuess !== yearsVegan) {
 vegan();
 
 
-  function labrodorite(){
-    var favStones = ['herkimer', 'black tourmaline', 'apatite', 'labrodorite', 'amethyst', 'selenite', 'pyrite'];
-    var sixGuesses = 0;
+function labrodorite(){
+  var favStones = ['herkimer', 'black tourmaline', 'apatite', 'labrodorite', 'amethyst', 'selenite', 'pyrite'];
+  var sixGuesses = 0;
 
-    for(var s=0; s < 6; s++) {
-      var stones = prompt('I\'ve been a rock hound since I was a kid.  Can you guess one of my favorite gemstones? You have six guesses.');
-      if ((favStones.indexOf(stones.toLowerCase())!== -1) && (sixGuesses < 6)) {
-        alert('You\'re right, ' + stones + ' is one of my favorites!  My other gemstones include herkimer, black tourmaline, apatite, labrodorite, amethyst, selenite, and pyrite.');
-        count++;
-      } else if (favStones.indexOf(stones.toLowerCase) === -1) {
-        alert('That\'s not one of my favorites.  Please guess again.');
-        sixGuesses++;
-      }
-    }
-    if(sixGuesses === 6){
-      alert('Sorry you ran out of guesses.  My favorites are herkimer, black tourmaline, apatite, labrodorite, amethyst, selenite, and pyrite.');
+  for(var s=0; s < 6; s++) {
+    var stones = prompt('I\'ve been a rock hound since I was a kid.  Can you guess one of my favorite gemstones? You have six guesses.');
+    if ((favStones.indexOf(stones.toLowerCase())!== -1) && (sixGuesses < 6)) {
+      alert('You\'re right, ' + stones + ' is one of my favorites!  My other gemstones include herkimer, black tourmaline, apatite, labrodorite, amethyst, selenite, and pyrite.');
+      count++;
+    } else if (favStones.indexOf(stones.toLowerCase) === -1) {
+      alert('That\'s not one of my favorites.  Please guess again.');
+      sixGuesses++;
     }
   }
+  if(sixGuesses === 6){
+    alert('Sorry you ran out of guesses.  My favorites stones are herkimer, black tourmaline, apatite, labrodorite, amethyst, selenite, and pyrite.');
+  }
+}
 
-  labrodorite();
+labrodorite();
 
-  count();
-  // prompt('What\'s Your Name?');
-  // // correctAnswers = [count];
-  alert('You got ' + count + ' questions out of eight correct!  Thanks for playing ' + userName + '!');
+
+userName();
+alert('You got ' + count + ' questions out of ' + totalQuestions + ' correct!  Thanks for planying ' + userName + '!');
+//Tells user how many questions they got correct // is not working
